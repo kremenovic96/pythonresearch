3f105dce243befb82268d5c8b3c2cffa3f35ce47
kremenovic96 <ranko.kremenovic@gmail.com>
2017-02-21 17:06:08 +0100

    finished hamlet hw

--- python_case_studies/datacamp/hamlet.py
+++ python_case_studies/datacamp/hamlet.py
@@ -44,3 +44,21 @@ for language in book_titles:
                 hamlets.loc[title_num] = language, distribution
                 title_num += 1
 
+colors = ["crimson", "forestgreen", "blueviolet"]
+handles, hamlet_languages = [], []
+for index in range(hamlets.shape[0]):
+    language, distribution = hamlets.language[index+1], hamlets.distribution[index+1]
+    dist = more_frequent(distribution)
+    plot, = plt.loglog(sorted(list(dist.keys())),sorted(list(dist.values()),
+        reverse = True), color = colors[index], linewidth = 2)
+    handles.append(plot)
+    hamlet_languages.append(language)
+plt.title("Word Frequencies in Hamlet Translations")
+xlim    = [0, 2e3]
+xlabel  = "Frequency of Word $W$"
+ylabel  = "Fraction of Words\nWith Greater Frequency than $W$"
+plt.xlim(xlim); plt.xlabel(xlabel); plt.ylabel(ylabel)
+plt.legend(handles, hamlet_languages, loc = "upper right", numpoints = 1)
+# show your plot using `plt.show`!
+plt.show()
+
